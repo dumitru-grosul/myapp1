bf6957d7d814f24770a1f2da730ff7d8b9496cf3
= <=>
2013-01-31 17:07:15 +0200

    Jsonp test

--- scripts.js
+++ scripts.js
@@ -1,23 +1,27 @@
-var deviceHeading, deviceName, deviceVersion;
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-	console.log('onLoad');
-	navigator.compass.getCurrentHeading(
-		function(heading){
-			deviceHeading = heading;
-		},
-		function(){
-			deviceHeading = 'Not defined';
-		}
-	);
-	deviceName = window.device.name;
-	deviceVersion = window.device.version;
-}
-
+localStorage.setItem('date', '123');
+console.log(localStorage);
+console.log(localStorage.getItem('date'));
 
 function doStuff(){
-	alert('Device Heading: ' + deviceHeading);
-	alert('Device Name: ' + deviceName);
+	document.getElementById('main').innerHTML = '<object type="type/html" data="http://google.com"><\/object>';
+	
 }
+function callme(data, status) {}
+
+(function($) {
+	$(document).ready(function() {
+		$.ajax({
+			type: 'GET',                                                                                                                                                                                                 
+			url: 'http://api.stackoverflow.com/1.1/stats?jsonp=callme', //'http://localhost/json_tmp.txt?jsonp=callme', // 
+			dataType: 'jsonp',                                                                                                                                                                                                
+			success: function(data) {
+				$('#hours_opened').html(data['statistics'][0]['answers_per_minute']);
+				$('#gallery_info').html(data['statistics'][0]['site']['name']);
+			},                                                                                                                                                                                       
+			error: function() { console.log('Uh Oh!'); },
+			jsonp: 'jsonp'   
+		});
+
+		return false;
+	});
+})(jQuery);
